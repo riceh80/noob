2557138f69dc0f7beeabc87be56329c803a29901
riceh80 <darrenhui05@gmail.com>
2025-02-06 23:17:08 +0000

    modified js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         setTimeout(() => {
             centerContainer.innerHTML = `
                 <div class="section centered-message">
-                    <h2 class="smaller-font"> Annyeong! Happy Valentine day! Thxs for being my valentine you cutie (Vivian Yang)! Really appearaciate it! (even though there isn't a choice hehe). 
-                    Sorry you might think I'm kinda annoying with the spam when the truth is I'm just a really clingy person
-                    when it comes to being with someone like you. Sorry I'm not there to physically be there for you during valentine however I can make it up as I've given you a gift... the labubu
-                    and a very nice date when I'm back hehe (ofc im paying so u dont have to since it is a date). I promise that I will make it all up for you and have fun with the website! Make sure if you need anything I am here for you even though we are 404 miles away, I will always support you! 
-                    Miss you cupcake <3</h2>
+                    <h2 class="smaller-font"> Annyeong! Happy Valentine day! Thxs for being my valentine you cutie! Really appearaciate it! (even though there isn't a choice hehe). </h2>
                     <button class="next-button">Next</button>
                 </div>`;
             centerContainer.style.animation = 'fade 800ms ease-in-out';
@@ -22,37 +18,30 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 800);
     };
 
-
-
     const handleNextButtonClick = () => {
         centerContainer.style.animation = 'fadeout 800ms ease-in-out';
         setTimeout(() => {
             centerContainer.innerHTML = `
-                <div class="new-page">
-                    <h2>Where would you like to go on a date?</h2>
-                    <div class="location-container">
-                        <div class="location-item">
-                            <input type="radio" id="location1" name="location" value="shooting range">
-                            <label for="location1">Shooting Range</label>
-                        </div>
-                        <div class="location-item">
-                            <input type="radio" id="location2" name="location" value="park">
-                            <label for="location2">Park</label>
-                        </div>
-                        <div class="location-item">
-                            <input type="radio" id="location3" name="location" value="cafe">
-                            <label for="location3">Cafe</label>
-                        </div>
-                        <div class="location-item">
-                            <input type="radio" id="location4" name="location" value="axe throwing">
-                            <label for="location4">Axe Throwing</label>
-                        </div>
-                        <div class="location-item">
-                            <input type="radio" id="location5" name="location" value="other">
-                            <label for="location5">Other:</label>
-                            <input type="text" id="other-location" name="other-location" placeholder="Enter location">
-                        </div>
-                    </div>
+                <div class="section centered-message">
+                    <h2 class="smaller-font"> Sorry you might think I'm kinda annoying with the spam when the truth is I'm just a really clingy person
+                    when it comes to being with someone like you. I really miss you, you know. Even 1 day feels like a year so knowing that, everytime you text me makes me feel happy hehe.
+                    It helps me with my stress and knowing that you are safe and happy makes me extremely happy. </h2>
+                    <button class="next-button">Next</button>
+                </div>`;
+            centerContainer.style.animation = 'fade 800ms ease-in-out';
+
+            const nextButton = document.querySelector('.next-button');
+            nextButton.addEventListener('click', handleNextButtonClick2);
+        }, 800);
+    };
+
+    const handleNextButtonClick2 = () => {
+        centerContainer.style.animation = 'fadeout 800ms ease-in-out';
+        setTimeout(() => {
+            centerContainer.innerHTML = `<div class="section centered-message">
+                    <h2 class="smaller-font"> Even though I won't be here for Valentine Day... I promise that I will make it up by setting us up on nice places for my entire spring break when I come back and when you are free.
+                    Can't wait to see your smile and your cute face again just really makes me happy just being beside you. My favorite and first Valentine gift that be given hehe. I hope I see you sooner, but I do go to buffalo so might take a while.
+                    Who knows maybe we can do something in call too if you like hehe. Miss you cupcake and have a Happy Valentine Day. <3 </h2>
                     <button class="next-button">Next</button>
                 </div>`;
             centerContainer.style.animation = 'fade 800ms ease-in-out';
@@ -62,30 +51,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 800);
     };
 
+    
     const handleNextButtonClick3 = () => {
-        centerContainer.style.animation = 'fadeout 800ms ease-in-out';
-        setTimeout(() => {
-            centerContainer.innerHTML = `
-                <div class="next-page">
-                    <h2>Since I'm not home... what day should our date be?</h2>
-                    <div class="date-container">
-                        <label for="date">Choose a date:</label>
-                        <input type="date" id="date" name="date">
-                    </div>
-                    <div class="time-container">
-                        <label for="time">Choose a time:</label>
-                        <input type="time" id="time" name="time">
-                    </div>
-                    <button class="next-button">Submit</button>
-                </div>`;
-            centerContainer.style.animation = 'fade 800ms ease-in-out';
-
-            const nextButton = document.querySelector('.next-button');
-            nextButton.addEventListener('click', handleNextButtonClick4);
-        }, 800);
-    };
-
-    const handleNextButtonClick4 = () => {
         centerContainer.style.animation = 'fadeout 800ms ease-in-out';
         setTimeout(() => {
             centerContainer.innerHTML = `
